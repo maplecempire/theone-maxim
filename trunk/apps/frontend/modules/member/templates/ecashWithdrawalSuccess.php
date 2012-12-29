9995437bca46bba86604bc96b4a34e5220d01f9f
r9projecthost@gmail.com <r9projecthost@gmail.com@f7fbe05d-9038-5ef2-c1f1-8312da453325>
2012-12-29 06:48:15 +0000

    upgrade package new solution

--- trunk/apps/frontend/modules/member/templates/ecashWithdrawalSuccess.php
+++ trunk/apps/frontend/modules/member/templates/ecashWithdrawalSuccess.php
@@ -118,8 +118,15 @@
                     <td><?php echo __('CP2 Withdrawal Amount'); ?></td>
                     <td>
                         <select name="ecashAmount" id="cbo_ecashAmount" tabindex="2">
+                            <option value="100">100</option>
                             <option value="200">200</option>
+                            <option value="300">300</option>
+                            <option value="400">400</option>
                             <option value="500">500</option>
+                            <option value="600">600</option>
+                            <option value="700">700</option>
+                            <option value="800">800</option>
+                            <option value="900">900</option>
                             <option value="1000">1,000</option>
                             <option value="1500">1,500</option>
                             <option value="2000">2,000</option>
